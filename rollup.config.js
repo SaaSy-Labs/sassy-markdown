2ed93cd23fc8dacd122571d9aeea087a1a5e3ee3
Julien Tremblay <tremblay.julien.pro@gmail.com>
2023-07-03 19:15:29 +0200

    chore(app): version 1.0.6

--- rollup.config.js
+++ rollup.config.js
@@ -17,7 +17,7 @@ export default [
         file: packageJson.main,
         format: "cjs",
         sourcemap: true,
-        name: "thedevdesigner-react-lib",
+        name: "@SaaSy-Labs/saasy-markdown",
       },
       {
         file: packageJson.module,
